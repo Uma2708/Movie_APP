d563bd7f228dc361604e269456d21bc26c1e3e44
Uma2708 <bhardwajuma001@gmail.com>
2022-09-10 11:34:16 +0530

    react-dom

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Components/Navbar';
+import Banner from './Components/Banner';
+import List from './Components/List';
+import Favourites from './Components/Favourites';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+// import { Fragment } from 'react';
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+      <Route path="/" element={<List/>}/>
+      <Route path="/fav" element={<Favourites/>}/>
+      </Routes>
+      {/* <Banner /> */}
+    </BrowserRouter>
+  );
+}
+
+export default App;
